05778cb13268acd98c38f7b5584ff84e2355314f
sc4n1a471 <martin.terhes@icloud.com>
2022-07-29 23:32:21 +0200

    Initial Commit

---
+++ NodeJS_Thingy_Minecraft/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  NodeJS_Thingy_Minecraft
+//
+//  Created by Martin Terhes on 7/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NodeJS_Thingy_Minecraft/NodeJS_Thingy_MinecraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  NodeJS_Thingy_MinecraftApp.swift
+//  NodeJS_Thingy_Minecraft
+//
+//  Created by Martin Terhes on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct NodeJS_Thingy_MinecraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
